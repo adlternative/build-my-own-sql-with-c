5f4a6ab924781a9451f2de5c15d75d1d634e1c3a
adl <1607364851@qq.com>
2020-11-05 23:14:50 +0800

    @

---
+++ build/CMakeFiles/mysqlite.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mysqlite.dir/main.c.o"
+  "CMakeFiles/mysqlite.dir/page_table.c.o"
+  "CMakeFiles/mysqlite.dir/row.c.o"
+  "mysqlite.pdb"
+  "mysqlite"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mysqlite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
